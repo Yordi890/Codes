f7a2ac47f73abcbea55806e4eb32a4c99fdc49a9
laZaro <lhdezp021126@gmail.com>
2023-10-23 04:04:33 +0200

    Changes 2

--- Cursos/Python/Curso Codigo facilito/15-funciones.py
+++ Cursos/Python/Curso Codigo facilito/15-funciones.py
@@ -16,3 +16,31 @@ print(animal)
 
 func = lambda x: x*1.8+32 
 print(func(32))
+
+def function2():
+    def function3():
+        print(list(i for i in range(10)))
+    return function3
+
+def decorator(func):
+    def new_funcion():
+        print("********")
+        func()
+        print("********")
+    return new_funcion
+    
+@decorator
+def function4():
+    print(list(i for i in range(10)))
+    
+
+function4()
+
+def function5():
+    for i in range(10):
+        yield i
+        
+print(i for i in function5())# por alguna razon que escapa a mi comprension asi no funciona
+
+for i in function5():
+    print(i)
